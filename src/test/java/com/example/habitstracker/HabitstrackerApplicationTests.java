ab5f23c16be1e94014e1dc4cedda74285473f652
AlibabaRazboinik <korall.sofiya@mail.ru>
2022-10-28 16:31:47 +0500

    юнит тест с моками и интеграционный тест на добавление двух одинаковых юзеров

--- src/test/java/com/example/habitstracker/HabitstrackerApplicationTests.java
+++ src/test/java/com/example/habitstracker/HabitstrackerApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.habitstracker;
 
+import com.example.habitstracker.dto.UserDTO;
+import com.example.habitstracker.exceptions.UserExistException;
+import com.example.habitstracker.services.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
 
 @SpringBootTest
+// @RunWith(SpringJUnit4ClassRunner.class)
 class HabitstrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserService userService;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users", "habit", "habit_list");
+    }
+
+
+    @Test
+    void checkSameNicks() {
+        
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNickname("nick");
+        userDTO.setPassword("123");
+        userDTO.setHabitListName("name");
+
+        userService.addUser(userDTO);
+        Assertions.assertThrows(UserExistException.class, () -> userService.addUser(userDTO));
+    }
 }
--- src/test/java/com/example/habitstracker/services/UserServiceTest.java
+++ src/test/java/com/example/habitstracker/services/UserServiceTest.java
@@ -4,31 +4,33 @@ import com.example.habitstracker.dto.UserDTO;
 import com.example.habitstracker.exceptions.UserExistException;
 import com.example.habitstracker.repository.HabitListRepository;
 import com.example.habitstracker.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Optional;
+
 public class UserServiceTest {
 
 
     /**
-     * Добавление пользователя с одинаковым ником
-     * Unit test -- проверка работы функции addUser
+     * Добавление пользователя с одинаковым ником через Mock unittest
      */
     @Test
     void checkSameNicks() {
         UserRepository userRepository = Mockito.mock(UserRepository.class);
-        UserService userService = Mockito.mock(UserService.class);
         HabitListRepository habitListRepository = Mockito.mock(HabitListRepository.class);
-        final UserDTO userDTO = new UserDTO();
+        UserService userService = new UserService(userRepository, habitListRepository);
+
+        UserDTO userDTO = new UserDTO();
         userDTO.setNickname("nick");
         userDTO.setPassword("123");
         userDTO.setHabitListName("name");
 
-        Mockito.when(userService.addUser(userDTO)).thenCallRealMethod();
-        Mockito.when(userService.addUser(userDTO)).thenThrow(UserExistException.class);
+        Mockito.when(userRepository.findByNickname("nick")).thenReturn(Optional.empty());
+        userService.addUser(userDTO);
 
-
+        Mockito.when(userRepository.findByNickname("nick")).thenThrow(UserExistException.class);
+        Assertions.assertThrows(UserExistException.class, () -> userService.addUser(userDTO));
     }
 }
