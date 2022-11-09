58eda7360896bc8ab04ab87fd60c04b165a096c8
lkorasik <lkorasik83@gmail.com>
2022-11-09 13:58:15 +0500

    Дописать UserMapper https://github.com/AlibabaRazboinik/habit_tracker/issues/66

--- src/main/java/com/example/habitstracker/mappers/UserMapper.java
+++ src/main/java/com/example/habitstracker/mappers/UserMapper.java
@@ -8,30 +8,29 @@ import com.example.openapi.dto.UserDTO;
 
 @Component
 public class UserMapper {
-
     public static User toEntity(UserDTO userDTO) {
         String username = userDTO.getUsername();
-
         String password = userDTO.getPassword();
+        String habitListName = userDTO.getHabitListName();
 
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-        user.setHabitList(new HabitList(userDTO.getHabitListName()));
+        user.setHabitList(new HabitList(habitListName));
+
         return user;
     }
 
     public static UserDTO toDTO(User user) {
-        Long id = user.getUserId();
-
         String username = user.getUsername();
-
         String password = user.getPassword();
-
+        HabitList habitList = user.getHabitList();
 
         UserDTO userDTO = new UserDTO();
         userDTO.setUsername(username);
         userDTO.setPassword(password);
+        if (habitList != null)
+            userDTO.setHabitListName(habitList.getName());
 
         return userDTO;
     }
