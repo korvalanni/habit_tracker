c78afe9b36643b697d80f57122e315e93aca79d1
lkorasik <lkorasik83@gmail.com>
2022-11-25 11:58:56 +0500

    Убрать аннотацию @EqualsAndHashCode у Entity https://github.com/AlibabaRazboinik/habit_tracker/issues/116

--- src/main/java/com/example/habitstracker/models/UserEntity.java
+++ src/main/java/com/example/habitstracker/models/UserEntity.java
@@ -1,26 +1,25 @@
 package com.example.habitstracker.models;
 
-import javax.persistence.*;
-
 import com.example.habitstracker.constants.TableNameConstants;
 import com.example.habitstracker.models.listeners.UserListener;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = TableNameConstants.USER)
 @EntityListeners(UserListener.class)
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 public class UserEntity implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Long userId;
 
     private String username;
@@ -43,5 +42,18 @@ public class UserEntity implements Cloneable {
     @Override
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserEntity that = (UserEntity) o;
+        return userId != null && Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
